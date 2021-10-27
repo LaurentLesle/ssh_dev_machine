9e231f2f2f090294adefd346d8463eab0885818b
lolorol <LaurentLesle@users.noreply.github.com>
2021-10-27 14:59:30 +0800

    Update command

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,8 @@
 # wsl --install -d Ubuntu
 ubuntu install
 
-$wsl_profile_path =  wsl wslpath (Get-ChildItem Env:USERPROFILE).value.replace('\', '\\') | Out-String
-ubuntu run /mnt/c/Users/lalesle/git/github.com/LaurentLesle/ssh_dev_machine/setup.sh -e wsl_profile_path=$wsl_profile_path
+$wsl_profile_path =  wsl -d Ubuntu wslpath (Get-ChildItem Env:USERPROFILE).value.replace('\', '\\') | Out-String
+$wsl_current_path = wsl -d Ubuntu wslpath (& write-output $pwd).path.replace('\', '\\') | Out-String
+ubuntu run $wsl_current_path/setup.sh -e wsl_profile_path=$wsl_profile_path
 
 code --install-extension ms-vscode-remote.remote-ssh --force
